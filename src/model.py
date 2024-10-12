787abcb454021a7cc1548a80d782c5efea9984a0
Enlysure <enlysure@gmail.com>
2024-10-12 21:29:40 +0800

    fox: PosixPath error

--- src/model.py
+++ src/model.py
@@ -10,7 +10,12 @@ ROOT_PATH: Path = Path(__file__ + '/../..').resolve()
 @dataclass
 class BaseModel:
     def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+        def default(o):
+            if isinstance(o, Path):
+                return str(o)
+            return o.__dict__
+        
+        return json.dumps(self, default=default, sort_keys=True, indent=4)
 
 
 # 用户信息结构:
