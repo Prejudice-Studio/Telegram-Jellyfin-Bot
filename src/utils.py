f7535760f05f9ed2f8d59e0f1011ec4fd3d355c0
Enlysure <enlysure@gmail.com>
2024-10-13 18:44:36 +0800

    fix: expect

--- src/utils.py
+++ src/utils.py
@@ -5,17 +5,20 @@ import pytz
 
 
 def convert_to_china_timezone(time_data: Optional[int | str] = None) -> str:
-    if not time_data or time_data == "N/A":
-        return "N/A"  # 或其他默认值
-    if isinstance(time_data, (int, float)):
-        utc_time = datetime.fromtimestamp(time_data, tz=timezone.utc)
-    elif isinstance(time_data, str):
-        utc_time = datetime.fromisoformat(time_data.replace("Z", "+00:00"))
-    else:
-        utc_time = datetime.now().astimezone(timezone.utc)
-    
-    china_timezone = pytz.timezone('Asia/Shanghai')
-    
-    china_time = utc_time.astimezone(china_timezone)
-    
-    return china_time.strftime('%Y-%m-%d %H:%M:%S')
+    try:
+        if not time_data or time_data == "N/A":
+            return "N/A"  # 或其他默认值
+        if isinstance(time_data, (int, float)):
+            utc_time = datetime.fromtimestamp(time_data, tz=timezone.utc)
+        elif isinstance(time_data, str):
+            utc_time = datetime.fromisoformat(time_data.replace("Z", "+00:00"))
+        else:
+            utc_time = datetime.now().astimezone(timezone.utc)
+        
+        china_timezone = pytz.timezone('Asia/Shanghai')
+        
+        china_time = utc_time.astimezone(china_timezone)
+        
+        return china_time.strftime('%Y-%m-%d %H:%M:%S')
+    except Exception as e:
+        return time_data
