05a2c77d145a9f11493292a1dae385ccb753c7b2
Enlysure <Enlysure@gmail.com>
2025-04-11 18:08:02 +0800

    fix: database url

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -7,7 +7,7 @@ from src.config import Config
 
 def create_database(database_name: str, model):
     os.makedirs(Config.DATABASES_DIR, exist_ok=True)
-    database_url = f"sqlite:///{Config.DATABASES_DIR / f'{database_name}.db'}"
+    database_url = f"sqlite:///{os.path.join(Config.DATABASES_DIR, f'{database_name}.db')}"
     engine = create_engine(database_url)
     model.metadata.create_all(engine)
     connection = engine.connect()
