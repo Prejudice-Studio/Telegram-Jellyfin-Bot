c255af24f998a1290d679b4df5dd37e90f8730a1
Enlysure <Enlysure@gmail.com>
2025-05-11 14:56:12 +0800

    ✨ 修改(src/bot/msg.py): 更新转发消息

--- src/bot/msg.py
+++ src/bot/msg.py
@@ -4,5 +4,5 @@ from telegram.ext import ContextTypes
 
 async def forward_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.channel_post and update.channel_post.chat_id == -1002370650432:
-        await context.bot.copyMessage(chat_id=-1002531979765, message_thread_id=1724, from_chat_id=-1002370650432,
+        await context.bot.copyMessage(chat_id=-1002531979765, message_thread_id=1632, from_chat_id=-1002370650432,
                                       message_id=update.channel_post.message_id)
